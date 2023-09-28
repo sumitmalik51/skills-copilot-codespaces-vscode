570f1935b2253d1e96f264c5d365c5104740f7c7
Sumit Malik <sumitmalik51@gmail.com>
2023-09-28 13:42:08 +0000

    Copilot third commit

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
